4cf390bf0cbbc0d11bced963326d090f97715f44
DESKTOP-59A72GD\it <dbtmdlee12@google.com>
2025-09-04 14:48:59 +0900

    25/09/03 WordQuiz 풀기 어려웠음.

--- src/assignment03/WordQuiz.java
+++ src/assignment03/WordQuiz.java
@@ -1,5 +1,6 @@
 package assignment03;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -46,18 +47,18 @@ public class WordQuiz {
 		v.add(new Words("statue", "조각상"));
 	}
 	
-	private int makeExample(int ex[], int answerIndex) {
-		int n[] = {-1,-1,-1,-1};
-		int index;
+	private int makeExample(int ex[], int answerIndex) { //wordQuiz에서 한글 <보기>의 배열을 만든다.
+		int n[] = {-1,-1,-1,-1}; // 4개의 <보기>를 만들기 위한 배열 초기화
+		int index; // n[]에 v의 index 값을 저장한다.
 		for(int i=0; i<n.length; i++) {
 			do {
-				index = (int)(Math.random()*v.size());
-			} while(index == answerIndex || exists(n, index));
-			n[i] = index;
+				index = (int)(Math.random()*v.size()); //index는 0~16 사이의 값을 가진다. -> v의 값을 지정할 수 있는 index.
+			} while(index == answerIndex || exists(n, index)); //index가 answerIndex와 같거나 n[]에 이미 있는 index라면 다시 do로 돌아가서 새로운 index 생성.
+			n[i] = index; //while 조건을 벗어나는 index 값이라면 n[] i번째에 index 값을 저장한다.
 		}
 		
-		for(int i=0; i<n.length; i++) ex[i] = n[i];
-		return (int)(Math.random()*n.length);
+		for(int i=0; i<n.length; i++) ex[i] = n[i]; //-> n[]에 저장된 index 값을 ex[]에 저장한다.
+		return (int)(Math.random()*n.length); //-> makeExample() 메소드의 return 값. 해당 값은 0~3 사이의 랜덤 정수. <보기>에 정답이 지정될 위치(wordQuiz에서 example에 지정되는 정답 위치). 
 	}
 
 	private boolean exists(int[] n, int index) {
@@ -71,21 +72,78 @@ public class WordQuiz {
 	public void run() {
 		System.out.println("**** 영어 단어 테스트 프로그램 " + name + "입니다. ****");
 		while(true) {
-			System.out.println("단언테스트(1), 단어삽입(2), 종료(3)>> ");
+			System.out.println("단어테스트(1), 단어삽입(2), 종료(3)>> ");
 			try {
 				int menu = sc.nextInt();
 				switch(menu) {
-				case 1: //단어테스트
-				case 2: //단어 삽입
-				case 3: //종료
+				case 1: wordQuiz(); break;
+				case 2: insertWords(); break;
+				case 3: finish(); return;
 				default: System.out.println("잘못 입력했습니다.");
 				}
-			} 
+			} catch(InputMismatchException e) {
+				sc.next();
+				System.out.println("숫자를 입력하세요!!");
+			}
+			System.out.println();
+		}
+	}
+	
+	private void insertWords() {
+		System.out.println("영어 단어에 그만을 입력하면 입력을 종료합니다.");
+		while(true) {
+			System.out.println("영어 한글 입력 >>");
+			String engWord = sc.next();
+			if(engWord.equals("그만"))
+				break;
+			
+			String korWord = sc.next();
+			v.add(new Words(engWord, korWord));
+		}
+	}
+	
+	private void finish() {
+		System.out.println("\"" + name + "\"를 종료합니다.");
+		sc.close();
+	}
+	
+	private void wordQuiz() {
+		System.out.println("현재 " + v.size() + "개의 단어가 들어 있습니다. -1을 입력하면 테스트를 종료합니다.");
+		while(true) {
+			int answerIndex = (int)(Math.random()*v.size()); //0~16 사이의 숫자를 랜덤하게 뽑는다.
+			String eng = v.get(answerIndex).getEng(); //v에 저장된 answerIndex(0~16) 위치에 있는 english를 eng에 저장한다.
+			
+			int example[] = new int [4];
+			
+			int answerLoc = makeExample(example, answerIndex); //-> example[]에 answerIndex를 제외한 v의 index가 저장됨. return 값으로 0~3 사이의 숫자가 answerLoc에 저장됨.
+			example[answerLoc] = answerIndex; //-> example[]에 answerLoc(0~3) 위치에 answerIndex가 저장됨.
+			
+			System.out.println(eng + "?");
+			
+			for(int i=0; i<example.length; i++)
+				System.out.println("(" + (i+1) + ")" + v.get(example[i]).getKor() + " ");
+			
+			System.out.print(":>");
+			try {
+				int in = sc.nextInt();
+				if(in == -1)
+					break;
+				
+				in--;
+				if(in == answerLoc)
+					System.out.println("Excellent!!");
+				else
+					System.out.println("No !!");
+			} catch(InputMismatchException e) {
+				sc.next();
+				System.out.println("숫자를 입력하세요 !!");
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		
+		WordQuiz quiz = new WordQuiz("명품영어");
+		quiz.run();
 	}
 
 }
